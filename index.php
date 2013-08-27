094be8cca90893e86fbe23b2ea79d7b041984324
randix0 <i@randix.info>
2013-08-27 16:17:51 +0300

    adding hashchange

--- index.php
+++ index.php
@@ -24,20 +24,41 @@
 <body>
 
 <nav>
+    <h2>Plain links</h2>
     <a id="prev" href="/prev">prev</a>
     <a href="/">index</a>
     <a id="next" href="/next">next</a>
 </nav>
 
 <nav>
+    <h2>Links with class="x". If H.config.useStates set TRUE - do ajax load</h2>
     <a class="x" href="/prev">x-prev</a>
     <a class="x" href="/">x-index</a>
     <a class="x" href="/next">x-next</a>
 </nav>
 
+<nav>
+    <h2>Links with href="#!". If H.config.useStates set TRUE - do ajax load</h2>
+    <a href="#!/prev">x-prev</a>
+    <a href="#!/">x-index</a>
+    <a href="#1/next">x-next</a>
+</nav>
+
 <section id="mainContent">
     <?=($action)?'$action is set to '.$action:'$action not set'?>
 </section>
+
+<!-- for test simple #foo links
+<div style="background: #ccc; height: 1000px;">
+    just spacer
+    <br>
+    <a href="#!foo">#!foo</a>
+    <a href="#foo">goto foo</a>
+</div>
+<div id="foo">
+    foo=bar
+</div>
+-->
 
 </body>
 </html>
--- js/h.api.js
+++ js/h.api.js
@@ -1,26 +1,68 @@
 H = {
+    config: {
+        useStates: false,
+        useHashes: false
+    },
     html5ready: null,
+    hashReady: null,
     currentsURI: null,
     contentDOM: '#mainContent',
     html5supports: function(){
         return !!(window.history && history.pushState);
     },
+    hashSupports: function(){
+        if (("onhashchange" in window)) return true;
+        else return false;
+    },
     init: function(){
         H.currentsURI = location.pathname;
-        if (!H.html5ready) H.html5ready = H.html5supports;
+        H.html5ready = H.html5supports();
+        if (!H.html5ready) H.config.useStates = false;
+        H.hashReady = H.hashSupports();
+        if (!H.hashReady) H.config.useHashes = false;
+        H.hashchange();
         H.binder();
         console.log('H inited');
     },
     binder: function(){
-        $('.x').bind('click', function(e){
-            //console.log(this.href);
-            H.ajaxGetContent(this.href);
-            history.pushState(null, null, this.href);
-            e.preventDefault();
-        });
+        if (H.config.useStates){
+            $('.x').bind('click', function(e){
+                //console.log(this.href);
+                H.ajaxGetContent(this.href);
+                history.pushState(null, null, this.href);
+                e.preventDefault();
+            });
+
+            window.setTimeout(function() {
+                window.addEventListener("popstate", H.popstate, false);
+            }, 1);
+        }
+        if (H.config.useHashes){
+            if (!H.config.useStates) {
+                $('.x').bind('click', function(e){
+                    window.location.hash = '#!'+$(this).attr('href');
+                    e.preventDefault();
+                });
+            }
+
+            window.setTimeout(function() {
+                window.addEventListener("hashchange", H.hashchange, false);
+            }, 1);
+        }
+    },
+    popstate: function(){
+        if (H.currentsURI != location.pathname) {
+            H.ajaxGetContent(location.pathname);
+        }
+    },
+    hashchange: function(){
+        location.hash.substr(0,2);
+        if (location.hash.substr(0,3) == '#!/') {
+            link = location.hash.replace('#!','');
+            H.ajaxGetContent(link);
+        }
     },
     ajaxGetContent: function(href){
-        console.log('H ajaxGetContent');
         $.ajax({
             url: href,
             type: 'GET',
@@ -28,7 +70,7 @@ H = {
             success: function(data){
                 console.log(data);
                 $(H.contentDOM).html(data);
-                H.currentsURI = location.pathname;
+                H.currentsURI = data;
             }
         });
     }
@@ -36,14 +78,4 @@ H = {
 
 $(document).ready(function(){
     H.init();
-    if (!H.html5ready) { return; }
-    window.setTimeout(function() {
-        window.addEventListener("popstate", function(e) {
-            if (H.currentsURI != location.pathname) {
-                H.ajaxGetContent(location.pathname);
-                H.currentsURI = location.pathname;
-                console.log(location.pathname);
-            }
-        }, false);
-    }, 1);
 });
